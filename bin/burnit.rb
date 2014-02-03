78008585454f3a056b01f69eb7a2a914ce3b7c23
jesseshieh <jesse.shieh.pub@gmail.com>
2014-02-02 20:03:10 -0800

    comments, add other asset classes

--- bin/burnit.rb
+++ bin/burnit.rb
@@ -1,23 +1,32 @@
 require 'burn_plan'
 
+# construct our "world"
 federal_reserve = BurnPlan::FederalReserve.new(0.031, 0.043)
 
 economy = BurnPlan::EconomyBuilder.new
-  .add_asset_class(BurnPlan::AssetClass.new('Large Company Stock', 0.123, 0.202))
-  .add_asset_class(BurnPlan::AssetClass.new('Long Term Government Bonds', 0.055, 0.057))
+  .add_asset_class(BurnPlan::AssetClass.new('Large Company Stock',        0.1230, 0.2020))
+  .add_asset_class(BurnPlan::AssetClass.new('Small Company Stocks',       0.1740, 0.3290))
+  .add_asset_class(BurnPlan::AssetClass.new('Long-term Corporate Bonds',  0.0620,	0.0850))
+  .add_asset_class(BurnPlan::AssetClass.new('Long Term Government Bonds', 0.0550,	0.0570))
+  .add_asset_class(BurnPlan::AssetClass.new('U.S. Treasury Bills',        0.0380,	0.0310))
   .build
 
 portfolio = BurnPlan::PortfolioBuilder.new
-  .add_asset(BurnPlan::Asset.new('Large Company Stock', 1_000))
+  .add_asset(BurnPlan::Asset.new('Large Company Stock',        1_000))
+  .add_asset(BurnPlan::Asset.new('Small Company Stocks',       1_000))
+  .add_asset(BurnPlan::Asset.new('Long-term Corporate Bonds',  1_000))
   .add_asset(BurnPlan::Asset.new('Long Term Government Bonds', 1_000))
+  .add_asset(BurnPlan::Asset.new('U.S. Treasury Bills',        1_000))
   .build
 
+# set our life strategies
 distribution_strategy = BurnPlan::DistributionStrategy::NoDistributionStrategy.new
 
+# run the simulations
 life = BurnPlan::Life.new(portfolio, 70, economy, federal_reserve, distribution_strategy)
-
 monte_carlo = BurnPlan::MonteCarlo.new(100, life)
 results = monte_carlo.run
 
+# get the results
 puts results.mean
 puts results.num_zeros
