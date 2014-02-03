d214c964f1fd5d0ce611fab9cf738d86d37ec36d
jesseshieh <jesse.shieh.pub@gmail.com>
2014-02-02 17:12:10 -0800

    move random gaussian testing to random gaussian spec proper assetclass spec

--- spec/burn_plan/asset_class_spec.rb
+++ spec/burn_plan/asset_class_spec.rb
@@ -3,27 +3,16 @@ require 'spec_helper'
 describe BurnPlan::AssetClass do
   let(:mean) { 0.10 }
   let(:stddev) { 0.20 }
+  let(:random_gaussian) { double('random gaussian', {
+      :rand => 0.99
+  }) }
 
   subject { described_class.new("fake-name", mean, stddev) }
 
   describe '#next' do
-    it 'produces expected mean and stddev' do
-      results = []
-      n = 10_000
-
-      n.times do
-        results << subject.next
-      end
-      sum = results.inject(0) {|sum, result| sum += result}
-      mean = (1.0 * sum / n)
-      mean.should be_within(0.01).of 0.1
-
-      numerator = 0
-      results.each do |result|
-        numerator += (result - mean) ** 2
-      end
-      stddev = Math.sqrt(1.0 * numerator / n)
-      stddev.should be_within(0.01).of 0.2
+    it 'produces the next return' do
+      BurnPlan::RandomGaussian.should_receive(:new).and_return(random_gaussian)
+      subject.next.should eq 0.99
     end
   end
 end
