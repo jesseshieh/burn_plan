9a50ee2d52deac093d2b03849752c668fe3839fa
jesseshieh <jesse.shieh.pub@gmail.com>
2014-02-26 19:48:54 -0800

    add some crude REIT and foreign stock asset classes

--- bin/burnit.rb
+++ bin/burnit.rb
@@ -9,12 +9,16 @@ require 'burn_plan'
 federal_reserve = BurnPlan::FederalReserve.new(0.031, 0.043)
 
 # from http://www.bogleheads.org/wiki/Historical_and_expected_returns
+# and https://blog.wealthfront.com/tax-efficient-reits-investing/
+# and http://www.swapmeetdave.com/Bible/USForeign.htm
 economy = BurnPlan::EconomyBuilder.new
   .add_asset_class(BurnPlan::AssetClass.new('Large Company Stock',        0.1230, 0.2020))
   .add_asset_class(BurnPlan::AssetClass.new('Small Company Stocks',       0.1740, 0.3290))
   .add_asset_class(BurnPlan::AssetClass.new('Long-term Corporate Bonds',  0.0620,	0.0850))
   .add_asset_class(BurnPlan::AssetClass.new('Long Term Government Bonds', 0.0550,	0.0570))
   .add_asset_class(BurnPlan::AssetClass.new('U.S. Treasury Bills',        0.0380,	0.0310))
+  .add_asset_class(BurnPlan::AssetClass.new('REITs',                      0.063,  0.18))
+  .add_asset_class(BurnPlan::AssetClass.new('Foreign Stocks',             0.15,   0.22))
   .build
 
 # TODO: maybe we shouldn't use floats for money. use int as cents instead with rounding
@@ -22,8 +26,10 @@ portfolio = BurnPlan::PortfolioBuilder.new
   .add_asset(BurnPlan::Asset.new('Large Company Stock',        1_000.0))
   .add_asset(BurnPlan::Asset.new('Small Company Stocks',       1_000.0))
   .add_asset(BurnPlan::Asset.new('Long-term Corporate Bonds',  0.0))
-  .add_asset(BurnPlan::Asset.new('Long Term Government Bonds', 500.0))
-  .add_asset(BurnPlan::Asset.new('U.S. Treasury Bills',        500.0))
+  .add_asset(BurnPlan::Asset.new('Long Term Government Bonds', 0.0))
+  .add_asset(BurnPlan::Asset.new('U.S. Treasury Bills',        0.0))
+  .add_asset(BurnPlan::Asset.new('REITs',                      0.0))
+  .add_asset(BurnPlan::Asset.new('Foreign Stocks',             1_000.0))
   .build
 
 # set our life strategies
@@ -38,7 +44,7 @@ rebalancing_strategy = BurnPlan::RebalancingStrategy::OptimalRebalancingStrategy
 num_years_to_live = 70
 life_factory = BurnPlan::LifeFactory.new(portfolio, num_years_to_live, economy, federal_reserve, distribution_strategy, rebalancing_strategy)
 
-num_simulations = 1_000
+num_simulations = 10_000
 monte_carlo = BurnPlan::MonteCarlo.new(num_simulations, life_factory)
 results = monte_carlo.run
 
